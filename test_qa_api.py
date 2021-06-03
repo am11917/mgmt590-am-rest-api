59115f3a83567a86de3b18085e333b6483bb9248
Ayush Maheshwari <69768815+am11917@users.noreply.github.com>
2021-06-03 19:34:36 -0400

    Update test_qa_api.py

--- test_qa_api.py
+++ test_qa_api.py
@@ -46,7 +46,7 @@ def test_list_answers():
     questionData = {"question": "who did holly matthews play in waterloo rd?",
                     "context": "She attended the British drama school East 15 in 2005,and left after winning a high-profile role in the BBC drama Waterloo Road, playing the bully Leigh-Ann Galloway.[6] Since that role, Matthews has continued to act in BBC's Doctors, playing Connie Whitfield; in ITV's The Bill playing drug addict Josie Clarke; and she was back in the BBC soap Doctors in 2009, playing Tansy Flack."}
     headers = {'Content-Type': 'application/json'} 
-    url2 = "?model=bert-tiny"
+    url2 = "/answer?model=bert-tiny"
     with mock.patch('psycopg2.connect') as mocksql: 
         mocksql.connect().cursor().fetchall.return_value = [(1622203201, 'bert-tiny', 'bully Leigh-Ann Galloway',  'who did holly matthews play in waterloo rd?',  "She attended the British drama school East 15 in 2005,and left after winning a high-profile role in the BBC drama Waterloo Road, playing the bully Leigh-Ann Galloway.[6] Since that role, Matthews has continued to act in BBC's Doctors, playing Connie Whitfield; in ITV's The Bill playing drug addict Josie Clarke; and she was back in the BBC soap Doctors in 2009, playing Tansy Flack.")] 
         response = client.post(url2, headers=headers, json=questionData)
